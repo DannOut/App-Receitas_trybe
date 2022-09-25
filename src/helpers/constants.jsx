ed85f0e0f7d78b85794e276f241e17c0a43a139e
DannOut <danielxdn@gmail.com>
2022-09-25 13:19:36 -0300

    adiantando requisitos

--- src/helpers/constants.jsx
+++ src/helpers/constants.jsx
@@ -20,3 +20,8 @@ export const TEST_ID_PASSWORD_INPUT = 'password-input';
 export const TEST_ID_LOGIN_SUBMIT = 'login-submit-btn';
 export const TEST_ID_FOOTER_DRINKS = 'drinks-bottom-btn';
 export const TEST_ID_FOOTER_MEALS = 'meals-bottom-btn';
+
+// * LOCAL STORAGE INFORMATIONS
+export const USER_KEY_LS = 'user';
+export const MEALS_TOKEN_KEY_LS = 'mealsToken';
+export const DRINKS_TOKEN_KEY_LS = 'drinksToken';
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,7 +7,11 @@ import {
   MIN_PASSWORD, MEALS_LINK, EMAIL, PASSWORD,
   TEST_ID_EMAIL_INPUT,
   TEST_ID_PASSWORD_INPUT,
-  TEST_ID_LOGIN_SUBMIT } from '../helpers/constants';
+  TEST_ID_LOGIN_SUBMIT,
+  USER_KEY_LS,
+  MEALS_TOKEN_KEY_LS,
+  DRINKS_TOKEN_KEY_LS,
+} from '../helpers/constants';
 import { saveLocalStorage } from '../helpers/localStorage';
 import InputLogin from '../components/login-components/Input-Login.component';
 import ButtonLogin from '../components/login-components/Button-Login.component';
@@ -29,9 +33,9 @@ function Login() {
   };
 
   const redirectBtn = () => {
-    saveLocalStorage('user', { email });
-    saveLocalStorage('mealsToken', 1);
-    saveLocalStorage('drinksToken', 1);
+    saveLocalStorage(USER_KEY_LS, { email });
+    saveLocalStorage(MEALS_TOKEN_KEY_LS, 1);
+    saveLocalStorage(DRINKS_TOKEN_KEY_LS, 1);
     history.push(MEALS_LINK);
   };
 
@@ -62,7 +66,6 @@ function Login() {
         isDisabled={ !formValidation() }
         onClick={ redirectBtn }
       />
-
     </main>
   );
 }
