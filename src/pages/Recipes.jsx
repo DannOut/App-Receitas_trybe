597799835d32bed5a419c4dc36c9ef2eaacad6f8
DannOut <danielxdn@gmail.com>
2022-09-25 13:22:24 -0300

    adiantando requisitos

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Meals from './Meals';
 import Drinks from './Drinks';
 // import PropTypes from 'prop-types';
 
 function Recipes() {
+  const { location: { pathname } } = useHistory();
+
   return (
     <main>
-      { (Meals) ? <Meals /> : <Drinks /> }
+      { (pathname === '/meals') ? <Meals /> : <Drinks /> }
     </main>
   );
 }
