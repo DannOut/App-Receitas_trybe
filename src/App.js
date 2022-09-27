e1ae2b973a6ed4acfe998e62655008c8f75383e8
DannOut <danielxdn@gmail.com>
2022-09-27 19:27:39 -0300

    Tests Filters Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginProvider from './context/providers/LoginProvider';
 import FetchProvider from './context/providers/FetchProvider';
@@ -27,24 +27,22 @@ import {
 
 function App() {
   return (
-    <BrowserRouter>
-      <LoginProvider>
-        <FetchProvider>
-          <Switch>
-            <Route exact path={ MAIN_LOGIN_PAGE } component={ Login } />
-            <Route exact path={ MEALS_LINK } component={ Recipes } />
-            <Route exact path={ DRINKS_LINK } component={ Recipes } />
-            <Route exact path={ PROFILE_LINK } component={ Profile } />
-            <Route exact path={ RCP_DETAILS_MEALS_LINK } component={ RecipeDetails } />
-            <Route exact path={ RCP_DETAILS_DRINKS_LINK } component={ RecipeDetails } />
-            <Route exact path={ RCP_IN_PROGRESS_MEALS } component={ RecipeInProgress } />
-            <Route exact path={ RCP_IN_PROGRESS_DRINKS } component={ RecipeInProgress } />
-            <Route exact path={ FAVORITE_RECIPES } component={ FavoriteRecipes } />
-            <Route exact path={ DONE_RECIPES } component={ DoneRecipes } />
-          </Switch>
-        </FetchProvider>
-      </LoginProvider>
-    </BrowserRouter>
+    <LoginProvider>
+      <FetchProvider>
+        <Switch>
+          <Route exact path={ MAIN_LOGIN_PAGE } component={ Login } />
+          <Route exact path={ MEALS_LINK } component={ Recipes } />
+          <Route exact path={ DRINKS_LINK } component={ Recipes } />
+          <Route exact path={ PROFILE_LINK } component={ Profile } />
+          <Route exact path={ RCP_DETAILS_MEALS_LINK } component={ RecipeDetails } />
+          <Route exact path={ RCP_DETAILS_DRINKS_LINK } component={ RecipeDetails } />
+          <Route exact path={ RCP_IN_PROGRESS_MEALS } component={ RecipeInProgress } />
+          <Route exact path={ RCP_IN_PROGRESS_DRINKS } component={ RecipeInProgress } />
+          <Route exact path={ FAVORITE_RECIPES } component={ FavoriteRecipes } />
+          <Route exact path={ DONE_RECIPES } component={ DoneRecipes } />
+        </Switch>
+      </FetchProvider>
+    </LoginProvider>
   );
 }
 
