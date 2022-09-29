f223f95f622c86f4a1ca25293f28fb935e034a29
Bruno Veiga <eubrunoveiga@gmail.com>
2022-09-29 20:52:49 -0300

    requisitos ste 53

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,11 +1,107 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header/Header';
+import { /* saveLocalStorage,  */getFromLocalStorage } from '../helpers/localStorage';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function FavoriteRecipes() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const [filter, setFilter] = useState([]);
+
+  useEffect(() => {
+    const favoriteRecipesFromLocalStorage = getFromLocalStorage('favoriteRecipes')
+     || [];
+    setFavoriteRecipes(favoriteRecipesFromLocalStorage);
+  }, []);
+
+  if (filter.length < favoriteRecipes.length) {
+    setFilter(favoriteRecipes);
+  }
+
+  const onClickHandler = ({ target }) => {
+    const filtering = target.value;
+    console.log(filter);
+    const filterFavoriteRecipes = filter
+      .filter(({ type }) => filtering === 'all' || type === filtering);
+    setFavoriteRecipes(filterFavoriteRecipes);
+  };
+
   return (
     <div>
       <Header />
-      <h1>Favorite Recipes</h1>
+      <div>
+        <button
+          type="button"
+          value="all"
+          onClick={ (e) => onClickHandler(e) }
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          value="meal"
+          onClick={ (e) => onClickHandler(e) }
+          data-testid="filter-by-meal-btn"
+        >
+          Meals
+        </button>
+        <button
+          type="button"
+          value="drink"
+          onClick={ (e) => onClickHandler(e) }
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+      </div>
+      {favoriteRecipes.map((
+        {
+          image,
+          category,
+          name,
+          nationality,
+          alcoholicOrNot,
+          type,
+        },
+        index,
+      ) => (
+        <div key={ `${index}` } className="done-recipe-card">
+          <h1
+            data-testid={ `${index}-horizontal-name` }
+          >
+            { name }
+          </h1>
+          <input
+            type="image"
+            src={ image }
+            alt={ name }
+            className="done-recipe-image"
+            // onClick={ () => console.log('teste') }
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <div
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            { type === 'meal' ? `${nationality} - ${category}` : alcoholicOrNot }
+          </div>
+          <input
+            type="image"
+            alt="favorite"
+            data-testid={ `${index}-horizontal-favorite-btn` }
+            // onClick={ () => console.log('teste') }
+            src={ blackHeartIcon }
+          />
+          <input
+            type="image"
+            src={ shareIcon }
+            alt="share"
+            // onClick={ () => console.log('teste') }
+            data-testid={ `${index}-horizontal-share-btn` }
+          />
+        </div>
+      ))}
+
     </div>
   );
 }
