4e62a997b8fc3bfb2e2fc35b045837c1e5226f72
Rodrigo Nunes <brksnunes@protonmail.com>
2022-09-29 16:50:38 -0300

    feat: :sparkles: requirement 58 & 59 done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from './Footer';
 import Header from '../components/Header/Header';
+import { getFromLocalStorage } from '../helpers/localStorage';
+import { USER_KEY_LS } from '../helpers/constants';
 
 // import PropTypes from 'prop-types';
 
 function Profile() {
+  const [userEmail, setUserEmail] = useState('');
+
+  useEffect(() => {
+    const { email } = getFromLocalStorage(USER_KEY_LS);
+    setUserEmail(email);
+  }, []);
+
   return (
     <div>
       <Header />
-      <h3 data-testid="profile-email">email</h3>
+      <h3 data-testid="profile-email">{ userEmail }</h3>
       <button
         type="button"
         data-testid="profile-done-btn"
