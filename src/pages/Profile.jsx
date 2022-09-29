2b42f6ec462a044a1340cb693df24a7fb2214768
Rodrigo Nunes <brksnunes@protonmail.com>
2022-09-29 16:43:39 -0300

    feat: :sparkles: requirement 57 done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,7 +8,25 @@ function Profile() {
   return (
     <div>
       <Header />
-      Profile
+      <h3 data-testid="profile-email">email</h3>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
