df240cce88c0a55fc0eab6d3d1cda3465db6f334
Rodrigo Nunes <brksnunes@protonmail.com>
2022-10-04 12:52:03 -0300

    style: :lipstick: Profile page styling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import Footer from './Footer';
 import Header from '../components/Header/Header';
+import profileIcon from '../images/profile.png';
 import { getFromLocalStorage } from '../helpers/localStorage';
 import { USER_KEY_LS, DONE_RECIPES, FAVORITE_RECIPES } from '../helpers/constants';
+import '../styles/Profile.css';
 
 // import PropTypes from 'prop-types';
 
@@ -24,39 +27,37 @@ function Profile() {
   return (
     <div>
       <Header />
-      <h3 data-testid="profile-email">{ userEmail }</h3>
-      <Link to={ DONE_RECIPES }>
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-        >
-          Done Recipes
-        </button>
-      </Link>
-      <Link to={ FAVORITE_RECIPES }>
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-        >
-          Favorite Recipes
-        </button>
-      </Link>
-      {/* <Link to="/">
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ logoutButtonHandler }
-        >
-          Logout
-        </button>
-      </Link> */}
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ logoutButtonHandler }
-      >
-        Logout
-      </button>
+      <div className="profile-top">
+        <img src={ profileIcon } alt="Profile Icon" />
+        <h4 data-testid="profile-email" className="display-6">{ userEmail }</h4>
+      </div>
+      <div className="profile-container mt-5">
+        <div className="d-grid gap-4 buttons-container">
+          <Link to={ DONE_RECIPES }>
+            <Button
+              variant="success"
+              style={ { width: '100%' } }
+            >
+              Done Recipes
+            </Button>
+          </Link>
+
+          <Link to={ FAVORITE_RECIPES }>
+            <Button
+              variant="warning"
+              style={ { width: '100%' } }
+            >
+              Favorite Recipes
+            </Button>
+          </Link>
+          <Button
+            variant="danger"
+            onClick={ logoutButtonHandler }
+          >
+            Logout
+          </Button>
+        </div>
+      </div>
       <Footer />
     </div>
   );
